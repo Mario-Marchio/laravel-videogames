a5b5344202d08e470bb51fe85519878b6f8dc546
TALEX1995 <tanase.alex@libero.it>
2023-09-04 12:19:13 +0200

    Added index and show videogame

---
+++ resources/views/admin/videogames/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1 class="mt-2">{{ $videogame->title }}</h1>
+  <img src="{{ $videogame->image }}" alt="{{ $videogame->title }}">
+  <p><strong>Release date :</strong> {{ $videogame->release_year }}</p>
+  <div class="d-flex flex-column">
+    <span><strong>Created : </strong>{{ $videogame->created_at }}</span>
+    <span><strong>Last Update : </strong>{{ $videogame->updated_at }}</span>
+  </div>
+  <div class="d-flex justify-content-between mt-3">
+    <a class="btn btn-warning mx-3" href="{{ route('admin.videogames.edit', $videogame) }}">Modifica</a>
+    <form action="#" method="POST">
+      @csrf
+      @method('DELETE')
+      <button class="btn btn-danger">Cancella</button>
+    </form>
+  </div>
+@endsection
