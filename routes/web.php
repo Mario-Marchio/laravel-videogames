71f225d10390b8da5159ebc98ded435fab978a14
Mario-Marchio <mariomarchio1610@gmail.com>
2023-09-04 11:40:55 +0200

    added controllers and routes for videogames

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Guest\HomeController as GuestHomeController;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\VideogameController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-})->name('guest.home');
+Route::get('/', [GuestHomeController::class, 'index'])->name('guest.home');
 
-Route::get('/admin', function () {
-    return view('admin.home');
-})->middleware(['auth', 'verified'])->name('admin.home');
-
+Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function () {
+    Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+    Route::resource('videogames', VideogameController::class);
+});
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
