71afbe4d919e94ea3340252c0f52bd0333f644cb
carloeusebi <carloeusebi@gmail.com>
2023-09-04 15:41:11 +0200

    Added logic for preview

--- resources/views/admin/videogames/form.blade.php
+++ resources/views/admin/videogames/form.blade.php
@@ -1,6 +1,8 @@
 <div class="card p-4">
 
-    <div class="row">
+    <div x-data="{
+        imagePreview: '{{ old('image', $videogame->image) }}'
+    }">
         @csrf
 
         {{-- TITLE --}}
@@ -16,49 +18,61 @@
         </div>
 
         {{-- IMAGE --}}
-        <div class="col-8">
-            <label for="image">Image</label>
-            <input type="text" class="form-control @error('image') is-invalid @enderror" id="image"
-                name="image" value="{{ old('image', $videogame->image) }}">
-            @error('image')
-                <div class="invalid-feedback">
-                    {{ $message }}
+        <div class="row my-3">
+
+            <div class="col-8">
+                <div class="mb-3">
+
+                    <label for="image">Image</label>
+                    <input x-model="imagePreview" type="text"
+                        class="form-control @error('image') is-invalid @enderror" id="image" name="image">
+                    @error('image')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-            @enderror
+
+                {{-- RELEASE YEAR --}}
+                <div class="mb-3">
+
+                    <label for="release_year">Release Year</label>
+                    <input type="date" class="form-control @error('release_year') is-invalid @enderror"
+                        id="release_year" name="release_year"
+                        value="{{ old('release_year', $videogame->release_year) }}">
+                    @error('release_year')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                {{-- RATE --}}
+                <div class="mb-3">
+
+                    <input type="text" class="form-control @error('rate') is-invalid @enderror" id="rate"
+                        name="rate" value="{{ old('rate', $videogame->rate) }}">
+                    @error('rate')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+
+            {{-- IMAGE PREVIEW --}}
+            <div class="col-4 d-flex justify-content-center ">
+                <img class="img-fluid w-full " :src="imagePreview" alt="Image Preview">
+            </div>
         </div>
 
-        <div class="col-4">
-            {{-- TODO IMAGEP PREVIEW --}}
-            <img class="img-fluid w-full" src="" alt="Image Preview">
-        </div>
 
-        {{-- RELEASE YEAR --}}
-        <div class="col-6">
-            <label for="release_year">Release Year</label>
-            <input type="date" class="form-control @error('release_year') is-invalid @enderror" id="release_year"
-                name="release_year" value="{{ old('release_year', $videogame->release_year) }}">
-            @error('release_year')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
 
-        {{-- RATE --}}
-        <div class="col-6">
-            <label for="rate">Average Rate</label>
-            <input type="text" class="form-control @error('rate') is-invalid @enderror" id="rate" name="rate"
-                value="{{ old('rate', $videogame->rate) }}">
-            @error('rate')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
-            @enderror
-        </div>
 
         {{-- BUTTON --}}
-        <div class="d-flex justify-content-end align-items-center my-3">
-            <button class="btn btn-primary">Save</button>
+        <div class="d-flex align-items-center my-3">
+            <button class="btn btn-primary px-5 py-3">Save</button>
         </div>
     </div>
 </div>
