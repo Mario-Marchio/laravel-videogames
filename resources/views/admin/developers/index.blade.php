8d37304525a139778273bc162b4bd9e9a0243974
Mario-Marchio <mariomarchio1610@gmail.com>
2023-09-08 16:14:56 +0200

    developer index added

--- resources/views/admin/developers/index.blade.php
+++ resources/views/admin/developers/index.blade.php
@@ -1,1 +1,48 @@
 @extends ('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Elenco Sviluppatori</h1>
+
+
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <div class="mt-4 d-flex flex-row-reverse">
+            <a href="{{ route('admin.developers.create') }}" class="btn btn-success">Add developer</a>
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>developer</th>
+                    <th>Anno Produzione</th>
+                    <th>Località</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($developers as $developer)
+                    <tr>
+                        <td>{{ $developer->label }}</td>
+                        <td>{{ $developer->year_production }}</td>
+                        <td>{{ $developer->location }}</td>
+                        <td>
+                            <a href="{{ route('admin.developers.show', $developer->id) }}" class="btn btn-info">Details</a>
+                            <a href="{{ route('admin.developers.edit', $developer->id) }}" class="btn btn-primary">Edit</a>
+                            <form action="{{ route('admin.developers.destroy', $developer->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
+    </div>
+@endsection
